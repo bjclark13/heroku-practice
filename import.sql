1f656ebc6a6c5c6e1e512408ef9974faddadd781
BJ Clark <bjclark13@outlook.com>
2021-02-18 17:21:32 -0500

    fix import.sql

--- import.sql
+++ import.sql
@@ -6,21 +6,21 @@ CREATE TABLE questions (
 CREATE TABLE answers (
 	id SERIAL PRIMARY KEY,
 	answer VARCHAR(255),
-	isCorrect BOOL,
+	is_correct BOOL,
 	question_id INT, 
 	FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 INSERT INTO questions (question) VALUES ('What is the real name of Batman?');
 
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (1, 'Bruce Wayne', true);
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (1, 'BJ Clark', false);
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (1, 'Clark Kent', false);
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (1, 'Abraham Lincoln', false);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (1, 'Bruce Wayne', true);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (1, 'BJ Clark', false);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (1, 'Clark Kent', false);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (1, 'Abraham Lincoln', false);
 
 INSERT INTO questions (question) VALUES ('What does SQL stand for');
 
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (2, 'SQuirreL', false);
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (2, 'Structured Query Language', true);
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (2, 'Seriously Questionable League', false);
-INSERT INTO ANSWERS (question_id, answer, isCorrect) VALUES (2, 'Stans Quiet Lagoon', false);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (2, 'SQuirreL', false);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (2, 'Structured Query Language', true);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (2, 'Seriously Questionable League', false);
+INSERT INTO ANSWERS (question_id, answer, is_correct) VALUES (2, 'Stans Quiet Lagoon', false);
